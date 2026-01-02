cfe9ae5b0f3f18898916895d72d90a4e2210e647
bhonemyintmyat <bonemyat@5123.com>
2026-01-02 13:56:45 +0630

    Added a factorial function

--- hello.py
+++ hello.py
@@ -1,9 +1,26 @@
-print("Hello, World")
-n = 5
-for i in range(0,n):
-    print(i)
+def main():
+    n=int(input("What's the number? "))
+    print("Factorial is", factorial(n))
+    print("Hello, World")
+    n = 5
+    for i in range(0,n):
+        print(i)
 
-while n != 0:
-    print("hi")
-    n -=1
+    while n != 0:
+        print("hi")
+        n -=1
+def factorial(n):
+    if n<=1:
+        return 1
+    ans = 1
+    while n>0:
+        ans = ans * n
+        n=n-1
+    return ans
+
+main()
+
+
+
+
 
