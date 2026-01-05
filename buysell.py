5f66a698e127d5b17ce619fa7bd9555dabde80c9
bhonemyintmyat <bonemyat@5123.com>
2026-01-05 22:24:35 +0630

    The ultimate program and I asked AI for the algorithm

--- buysell.py
+++ buysell.py
@@ -1,21 +1,13 @@
-prices = [3,2,6,5,1,3]
-max = prices[0]
-min = prices[0]
 
+
+prices = [7,6,5,4,3,2,1]
+min = prices[0]
+diff = 0
 for i in range(len(prices)):
-    if i == len(prices):
-        break
-    
-    if prices[i] > max:
-        max = prices[i]
-    if prices[i] <= min:
+    if prices[i] < min:
         min = prices[i]
-        if  prices.index(max) < prices.index(min):
-            max = min
-        else:
-            continue
-if prices.index(max) > prices.index(min):
-    print(max - min)
-else:
-    print(0)
+    elif prices[i] >= min:
+        if prices[i] - min >diff:
+            diff = prices[i] - min
+print(diff)
 
