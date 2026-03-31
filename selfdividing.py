dd6b5c4f7e17435c62a8a8313fd887d806996af3
bhonemyintmyat <bonemyat@5123.com>
2026-03-31 21:53:14 +0630

    Remove the elif part

--- selfdividing.py
+++ selfdividing.py
@@ -5,8 +5,6 @@ for i in range(left, right+1):
     digits_list = [int(digit) for digit in str(i)]
     if i < 10:
         c.append(i)
-    elif i >= 10 and i % 10 == 0:
-        continue
     else: 
         if all(a != 0 and i % a == 0 for a in digits_list):
             c.append(i)
