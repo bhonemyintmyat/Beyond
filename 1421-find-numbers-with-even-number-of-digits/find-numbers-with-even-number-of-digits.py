2421ff501ecc9b781c8240e35103d93c20e03444
Phone Myint Myat <123240736+bhonemyintmyat@users.noreply.github.com>
2026-04-29 21:34:55 +0630

    Time: 0 ms (100.00%) | Memory: 19.1 MB (92.66%) - LeetSync

---
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        n = 0
+        for i in nums:
+            if len(str(i)) % 2 == 0:
+                n += 1
+        return n
+        
