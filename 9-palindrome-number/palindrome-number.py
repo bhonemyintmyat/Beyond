8ddb84265d612d1f3466d5c853b5778cbc0741d0
Phone Myint Myat <123240736+bhonemyintmyat@users.noreply.github.com>
2026-06-26 20:41:09 +0630

    Time: 17 ms (11.10%) | Memory: 19.3 MB (52.90%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,20 +1,13 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x == 0:
+        rev = 0
+        temp = x
+        while x>0:
+            rev = rev * 10 + (x % 10)
+            x //= 10
+        if temp == rev:
             return True
-        if not (x < 0) and x%10 != 0:
-            new = x
-            rev = 0
-            #x=123
-            while new: 
-                #rev=23
-                rev = rev * 10 + (new % 10) 
-                #x=12
-                new //= 10
-            if x == new+rev:
-                return True
-            else:
-                return False
         else:
-             return False
+            return False
+                
         
